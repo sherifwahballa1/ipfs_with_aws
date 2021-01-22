fe5935c925e873776d79b44b98236038d059b479
sherifwahballa1 <sherifwahballa0@gmail.com>
2021-01-22 15:48:30 +0200

    backend

---
+++ index.js
@@ -0,0 +1,49 @@
+/* eslint-disable object-shorthand */
+const express = require("express");
+// eslint-disable-next-line new-cap
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const bodyParser = require(" ");
+const xxs = require('xss-clean');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan("tiny"));
+app.use(xxs());
+
+// set port
+app.set("port", PORT);
+
+app.use(express.static(`${__dirname }/views/admin-dashboard`));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./views/admin-dashboard/index.html'));
+});
+
+// redirect client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./views/admin-dashboard/index.html'));
+});
+
+const server = require("http").createServer(app);
+
+
+server.listen(app.get("port"), function () {
+  console.log(
+    `running on port : ${app.get("port")}`
+  );
+});
